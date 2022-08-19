43eb58100397c508d017a8b3f5409c5f297c9a39
christofferaakre <christoffer.corfield@gmail.com>
2022-08-19 21:10:35 +0200

    moved handle_keyboard_input out of engine

--- src/events.rs
+++ src/events.rs
@@ -3,27 +3,22 @@
 
 use glium::glutin::{event, event_loop};
 
-fn handle_keyboard_input(input: event::KeyboardInput) {
-    match input.virtual_keycode {
-        Some(keycode) => match keycode {
-            event::VirtualKeyCode::Escape => {
-                std::process::exit(0);
-            }
-            _ => {}
-        },
-        None => {}
-    }
-}
-
 /// Handles events from the glium event loop.
 /// # Examples
 /// ```rust
+///        fn handle_keyboard_input(glium::glutin::KeyboardInput) {
+///            /* --snip -- /*
+///        }
 ///        event_loop.run(move |ev, _, control_flow| {
-///            handle_events(ev, control_flow);
+///            handle_events(ev, control_flow, handle_keyboard_input);
 ///            /* --snip-- /*
 ///    });
 /// ```
-pub fn handle_events<T>(ev: event::Event<T>, _control_flow: &mut event_loop::ControlFlow) {
+pub fn handle_events<T>(
+    ev: event::Event<T>,
+    _control_flow: &mut event_loop::ControlFlow,
+    handle_keyboard_input: fn(event::KeyboardInput) -> (),
+) {
     match ev {
         event::Event::WindowEvent { event, .. } => match event {
             event::WindowEvent::CloseRequested => {
