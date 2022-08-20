886b19c7e27ed8f743bd2a766dc00109f8108b95
christofferaakre <christoffer.corfield@gmail.com>
2022-08-20 21:33:46 +0200

    keyboard input

--- src/events.rs
+++ src/events.rs
@@ -1,34 +1,60 @@
 //! Module for handling events such as window resizing,
 //! keyboard input, etc.
 
-use glium::glutin::{event, event_loop};
+use cgmath::Vector2;
+use glium::glutin::{
+    event::{self, ElementState, KeyboardInput, VirtualKeyCode},
+    event_loop,
+};
+use std::collections::HashSet;
 
-/// Handles events from the glium event loop.
+/// Handles events from the glium event loop:
+/// * Responds to window events such as resizing by reiszing the window.
+/// * Returns the keyboard event if there was one
+/// * Updates the HashSet storing the pressed keys
 /// # Examples
 /// ```rust
-///        fn handle_keyboard_input(glium::glutin::KeyboardInput) {
-///            /* --snip -- /*
-///        }
+///        // HashSet to store pressed keys in
+///        let mut pressed_keys: HashSet<VirtualKeyCode> = HashSet::new();
+///
 ///        event_loop.run(move |ev, _, control_flow| {
-///            handle_events(ev, control_flow, handle_keyboard_input);
+///            let keyboard_input = handle_events(ev, control_flow, &mut pressed_keys);
 ///            /* --snip-- /*
 ///    });
 /// ```
 pub fn handle_events<T>(
     ev: event::Event<T>,
-    _control_flow: &mut event_loop::ControlFlow,
-    handle_keyboard_input: fn(event::KeyboardInput) -> (),
-) {
+    pressed_keys: &mut HashSet<VirtualKeyCode>,
+) -> Option<event::KeyboardInput> {
     match ev {
         event::Event::WindowEvent { event, .. } => match event {
             event::WindowEvent::CloseRequested => {
                 std::process::exit(0);
             }
             event::WindowEvent::KeyboardInput { input, .. } => {
-                handle_keyboard_input(input);
+                if input.virtual_keycode == None {
+                    panic!("Keyboard input {:?} did not have a valid key code", input);
+                }
+                update_pressed_keys(input, pressed_keys);
+                Some(input)
             }
-            _ => {}
+            _ => None,
         },
-        _ => {}
-    };
+        _ => None,
+    }
+}
+
+fn update_pressed_keys(input: event::KeyboardInput, pressed_keys: &mut HashSet<VirtualKeyCode>) {
+    let keycode = input
+        .virtual_keycode
+        .unwrap_or_else(|| panic!("Keyboard input {:?} did not have a valid keycode", input));
+
+    // key pressed and wasn't pressed before
+    if input.state == ElementState::Pressed && !pressed_keys.contains(&keycode) {
+        pressed_keys.insert(keycode);
+    }
+    // key was pressed before and was released
+    if input.state == ElementState::Released && pressed_keys.contains(&keycode) {
+        pressed_keys.remove(&keycode);
+    }
 }
